b7b7a94a63762bdad43117bf2bbb7aa69fdb3997
fl4me <lixinzhuang1996@gmail.com>
2021-03-08 13:34:02 +0800

    a new commit

--- push.sh
+++ push.sh
@@ -1,10 +1,7 @@
-# LeetCode-
-
-echo "# LeetCode-" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
+echo "# LeetCode开始提交"
+git add .
+git commit -m "a new commit"
 git branch -M main
 git remote add origin https://github.com/XinzhuangL/LeetCode-.git
 git push -u origin main
-
+echo "# LeetCode提交结束"
