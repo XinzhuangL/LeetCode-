4496d1c60fa1963a01eddabe72f9832c073bc516
XinzhuangL <1063815703@qq.com>
2021-03-08 13:50:03 +0800

    remove push.sh

--- push.sh
+++
@@ -1,7 +0,0 @@
-echo "# LeetCode开始提交"
-git add .
-git commit -m "a new commit"
-git branch -M main
-git remote add origin https://github.com/XinzhuangL/LeetCode-.git
-git push -u origin main
-echo "# LeetCode提交结束"
